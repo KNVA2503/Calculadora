2d1c0b0157d784cc426890659bfa0c98b541507b
KNVA2503 <160932654+KNVA2503@users.noreply.github.com>
2024-02-22 13:03:27 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -7,9 +7,9 @@ int main()
 float a,b;
 int c;
 puts("ingresa dos numeros para operar con ellos\n");
-scanf("%d%d"&a,&b);
+scanf("%f%f"&a,&b);
 puts("Escoge una oocion\n 1-suma \n 2-resta \n 3-multiplicacion \n 4-division");
-scanf("%")
+scanf("%d"&c);
   switch(c){
 case 1:
   break;
